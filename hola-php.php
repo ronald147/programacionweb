689563693eae5098739c37aa68b2db24d35caf2a
ronald147 <rwilfredomamani@gmail.com>
2016-08-09 19:02:28 -0500

    Agregamos presentacion y comportamiento

--- hola-php.php
+++ hola-php.php
@@ -1,9 +1,45 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-</head>
-<body>
-	<h1>Hola PHP</h1>
-</body>
+	<head>
+		<title>Hola PHP</title>
+		<style type="text/css">
+			/*presentacion*/
+			h1{
+				color: peru;
+			}
+			p{
+				color: #adcbf7;
+			}
+			li:first-letter{
+				font-size: 2em;
+			}
+		</style>
+		<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<script type="text/javascript">
+			//comportamiento
+			alert("Hola "+prompt("Ingresa tu nombre"));
+		</script>
+		<h1>Hola PHP</h1>
+		<p>Dias de la semana</p>
+		<a class=" btn btn-success" href="https://www.google.com.pe/">Ir a google</a>
+		<ol>
+			<li>lunes</li>
+			<li>Martes</li>
+			<li>Miercoles</li>
+			<li>Jueves</li>
+			<li>Viernes</li>
+			<li>Sabado</li>
+			<li>Domingo</li>
+		</ol>
+
+	</body>
 </html>
