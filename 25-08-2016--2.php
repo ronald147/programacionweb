4890d16dccfd7f4572d0b4dec72eae2ff5b77af4
ronald147 <rwilfredomamani@gmail.com>
2016-08-25 18:10:50 -0500

    25-08-2016

---
+++ 25-08-2016--2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<?php  
+			/*TABLAS DE CODIGOS (Encoding)*/
+			/*echo "<p>".utf8_encode("Perú")."</p>";*/
+			echo "<p>".("Perú")."</p>";
+		?>
+	</body>
+</html>
---
+++ 25-08-2016--3.php
@@ -0,0 +1,12 @@
+﻿<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<?php  
+			/*TABLAS DE CODIGOS (Encoding)*/
+			echo "<p>".("Perú")."</p>";
+		?>
+	</body>
+</html>
